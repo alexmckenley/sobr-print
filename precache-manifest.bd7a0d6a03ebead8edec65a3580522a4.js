2253564d323aeb05e3e6d7fd49915e2c0390ab76
Alex Taylor (alta) <alta@fb.com>
2020-05-22 13:17:56 -0700

    Updates

---
+++ precache-manifest.bd7a0d6a03ebead8edec65a3580522a4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d57edea772297439790b673e628bf458",
+    "url": "/index.html"
+  },
+  {
+    "revision": "22b020599cecf89aef48",
+    "url": "/static/css/main.9d7b2879.chunk.css"
+  },
+  {
+    "revision": "b6f05bf7aaee92123fc2",
+    "url": "/static/js/2.8ffc83bc.chunk.js"
+  },
+  {
+    "revision": "b34d611d69419bc1205d1e94f9169aa7",
+    "url": "/static/js/2.8ffc83bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22b020599cecf89aef48",
+    "url": "/static/js/main.52ee5b71.chunk.js"
+  },
+  {
+    "revision": "b2c71c47b30635d35a4a",
+    "url": "/static/js/runtime-main.c9bfec85.js"
+  },
+  {
+    "revision": "a795824650039f8b2b297c816bf20fcb",
+    "url": "/static/media/logo512.a7958246.png"
+  }
+]);
