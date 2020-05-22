2cb9ad270c8c217b09519b7d731a0a67372d4815
Alex Taylor (alta) <alta@fb.com>
2020-05-22 09:37:10 -0700

    Add server

---
+++ sobr-print-server.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python3
+"""
+Very simple HTTP server in python for logging requests
+Usage::
+    ./sobr-print-server.py [<port>]
+"""
+from escpos.printer import Usb
+from http.server import BaseHTTPRequestHandler, HTTPServer
+from systemd.journal import JournaldLogHandler
+import logging
+import json
+
+# get an instance of the logger object this module will use
+logger = logger = logging.getLogger(__name__)
+
+# instantiate the JournaldLogHandler to hook into systemd
+journald_handler = JournaldLogHandler()
+
+# set a formatter to include the level name
+journald_handler.setFormatter(logging.Formatter(
+    '[%(levelname)s] %(message)s'
+))
+
+# add the journald handler to the current logger
+logger.addHandler(journald_handler)
+
+# optionally set the logging level
+logger.setLevel(logging.INFO)
+
+# Connect to USB printer
+p = Usb(0x0416, 0x5011, profile="POS-5890")
+
+def print_text(text):
+    p.text(text + "\n")
+
+class S(BaseHTTPRequestHandler):
+    def _set_response(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'application/json')
+        self.end_headers()
+
+    def do_GET(self):
+        logger.info("GET request,\nPath: %s\nHeaders:\n%s\n", str(self.path), str(self.headers))
+        self._set_response()
+        self.wfile.write(json.dumps(True).encode('utf-8'))
+
+    def do_POST(self):
+        content_length = int(self.headers['Content-Length']) # <--- Gets the size of data
+        post_data = self.rfile.read(content_length) # <--- Gets the data itself
+        text = post_data.decode('utf-8')
+        logger.info("POST request,\nPath: %s\nHeaders:\n%s\n\nBody:\n%s\n",
+                str(self.path), str(self.headers), text)
+        self._set_response()
+        self.wfile.write(json.dumps(True).encode('utf-8'))
+        print_text(text)
+        print_text("\n\n\n")
+
+def run(server_class=HTTPServer, handler_class=S, port=8888):
+    server_address = ('', port)
+    httpd = server_class(server_address, handler_class)
+    logger.info('Starting httpd...\n')
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        pass
+    httpd.server_close()
+    logger.info('Stopping httpd...\n')
+
+if __name__ == '__main__':
+    from sys import argv
+
+    if len(argv) == 2:
+        run(port=int(argv[1]))
+    else:
+        run()
